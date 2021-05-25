f5119e0e6a6699a2d9ec46f6c3ce0bef5fb9f5d2
Jason Bai <jasonweipo@gmail.com>
2021-05-25 10:03:08 +0800

    delete app-input component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { PaginatorComponent } from './components/paginator/paginator.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 import { ListComponent } from './components/list/list.component';
-import { InputComponent } from './components/input/input.component';
 import { ListItemComponent } from './components/list/list-item/list-item.component';
 
 
 
 @NgModule({
   declarations: [
-    // PaginatorComponent,
+    PaginatorComponent,
     ListComponent,
-    InputComponent,
     ListItemComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    // PaginatorComponent,
+    PaginatorComponent,
     ListComponent,
-    InputComponent,
     ListItemComponent
   ]
 })
